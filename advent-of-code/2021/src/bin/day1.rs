37185b120a1c1d375375a71b151387d981dfab88
Tymoteusz Makowski <t@makowski.sh>
2022-06-08 21:47:58 +0200

    feat: add aoc 2021 day 1 part 2

--- advent-of-code/2021/src/bin/day1.rs
+++ advent-of-code/2021/src/bin/day1.rs
@@ -11,11 +11,8 @@ fn get_input() -> &'static str {
 263";
 }
 
-fn main() {
-    let result = get_input()
-        .lines()
-        .flat_map(str::parse::<i32>)
-        .collect::<Vec<i32>>()
+fn n_increments(x: &Vec<i32>) -> i32 {
+    return x
         .iter()
         .fold((0, None), |mut acc, item| {
             if let Some(x) = acc.1 {
@@ -27,6 +24,22 @@ fn main() {
             return acc;
         })
         .0;
+}
 
-    println!("{}", result);
+fn main() {
+    let numbers = get_input()
+        .lines()
+        .flat_map(str::parse::<i32>)
+        .collect::<Vec<i32>>();
+
+    println!("Part 1: {}", n_increments(&numbers));
+
+    let numbers = numbers
+        .iter()
+        .skip(2)
+        .enumerate()
+        .map(|(i, _)| -> i32 { numbers[i..=i + 2].iter().sum() })
+        .collect::<Vec<i32>>();
+
+    println!("Part 2: {}", n_increments(&numbers));
 }
